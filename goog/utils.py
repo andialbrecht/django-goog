e95a0eb6929e1e15d9d9cc6d38e0d54fdc1d630d
Andi Albrecht <albrecht.andi@gmail.com>
2012-01-04 18:04:05 +0100

    Add TODO and provide new default.

--- goog/utils.py
+++ goog/utils.py
@@ -13,7 +13,9 @@ from django.contrib.staticfiles import finders
 from django.core.exceptions import ImproperlyConfigured
 
 
+# TODO(andi) Make this a setting.
 CLOSURE_LIB_URL = 'http://closure-library.googlecode.com/files/closure-library-20110323-r790.zip'
+#CLOSURE_LIB_URL = 'http://closure-library.googlecode.com/files/closure-library-20111110-r1376.zip'
 CLOSURE_COMPILER_URL = 'http://closure-compiler.googlecode.com/files/compiler-latest.zip'
 
 log = logging.getLogger('goog')
